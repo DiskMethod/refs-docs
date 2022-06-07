f1ee67b2e2a16bbbfbe50339223f39ef5f7c4dd6
Lucas Chagas <lucas.w.chagas@gmail.com>
2022-06-06 23:46:18 -0400

    Forward refs

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import "./App.css";
+import React, { useRef, useEffect } from "react";
+
+// import "./App.css";
 // import MyComponent from "./components/MyComponent";
 // import CustomTextInput from "./components/CustomTextInput";
 // import AutoFocusTextInput from "./components/AutoFocusTextInput";
 // import Parent from "./components/MyFunctionComponent";
-import CustomTextInputFunction from "./components/CustomTextInputFunction";
+// import CustomTextInputFunction from "./components/CustomTextInputFunction";
+import FancyButton from "./components/FancyButton";
 
 function App() {
+  const ref = useRef();
+
+  useEffect(() => {
+    console.log(ref.current);
+  }, []);
+
   return (
     <>
-      <CustomTextInputFunction />
+      <FancyButton ref={ref}>Click me!</FancyButton>
     </>
   );
 }
